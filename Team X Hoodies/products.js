48ec82ffdc18687d46b073dc5f4ce19bb029df07
GavinMangroo <gavinmangroo@gmail.com>
2023-03-30 11:59:00 -0400

    Added Team X Website

---
+++ Team X Hoodies/products.js
@@ -0,0 +1,224 @@
+
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementsByClassName("product-link-1");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+for (var i = 0; i < btn.length; i++) {
+  btn[i].onclick = function() {
+    modal.style.display = "block";
+    document.querySelector(".product-image img").src = this.querySelector("img").src;
+    document.getElementById("product-name").innerHTML = this.querySelector(".overlay h1").innerHTML;
+    document.getElementById("product-price").innerHTML = this.querySelector(".overlay p").innerHTML;
+    document.getElementById("product-description").innerHTML = "This hoodie features a graphic print of an astronaut sitting on a swing that is being held up by the moon, creating an eye-catching design that is sure to turn heads. Made from high-quality materials, this hoodie is soft, warm and durable, making it ideal for everyday wear. The black and white color scheme gives it a modern, minimalist look that will complement any outfit.";
+  }
+}
+
+/*-------------------------------------------------------*/
+// Get the button that opens the modal
+var btn = document.getElementsByClassName("product-link-2");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+for (var i = 0; i < btn.length; i++) {
+  btn[i].onclick = function() {
+    modal.style.display = "block";
+    document.querySelector(".product-image img").src = this.querySelector("img").src;
+    document.getElementById("product-name").innerHTML = this.querySelector(".overlay h1").innerHTML;
+    document.getElementById("product-price").innerHTML = this.querySelector(".overlay p").innerHTML;
+    document.getElementById("product-description").innerHTML = "This hoodie features a graphic print of an abstract geometrical pattern with colorful borders, creating an eye-catching design that is sure to turn heads. Made from high-quality materials, this hoodie is soft, warm and durable, making it ideal for everyday wear. The black and white color scheme gives it a modern, minimalist look that will complement any outfit.";
+  }
+}
+
+
+/*------------------------------------------------------------------------------------------------------*/
+// Get the button that opens the modal
+var btn = document.getElementsByClassName("product-link-3");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+for (var i = 0; i < btn.length; i++) {
+  btn[i].onclick = function() {
+    modal.style.display = "block";
+    document.querySelector(".product-image img").src = this.querySelector("img").src;
+    document.getElementById("product-name").innerHTML = this.querySelector(".overlay h1").innerHTML;
+    document.getElementById("product-price").innerHTML = this.querySelector(".overlay p").innerHTML;
+    document.getElementById("product-description").innerHTML = "This hoodie features a graphic print of a static-like design split in black and white, creating an eye-catching design that is sure to turn heads. Made from high-quality materials, this hoodie is soft, warm and durable, making it ideal for everyday wear. The black and white color scheme gives it a modern, minimalist look that will complement any outfit.";
+  }
+}
+/*--------------------------------------------------------------------------------------------------------*/
+// Get the button that opens the modal
+var btn = document.getElementsByClassName("product-link-4");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+for (var i = 0; i < btn.length; i++) {
+  btn[i].onclick = function() {
+    modal.style.display = "block";
+    document.querySelector(".product-image img").src = this.querySelector("img").src;
+    document.getElementById("product-name").innerHTML = this.querySelector(".overlay h1").innerHTML;
+    document.getElementById("product-price").innerHTML = this.querySelector(".overlay p").innerHTML;
+    document.getElementById("product-description").innerHTML = "This hoodie features a graphic print of three contrasting colors diagnolly positioned from eachother, creating an eye-catching design that is sure to turn heads. Made from high-quality materials, this hoodie is soft, warm and durable, making it ideal for everyday wear. The black and white color scheme gives it a modern, minimalist look that will complement any outfit.";
+  }
+}
+/*--------------------------------------------------------------------------------------------------------*/
+// Get the button that opens the modal
+var btn = document.getElementsByClassName("product-link-5");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+for (var i = 0; i < btn.length; i++) {
+  btn[i].onclick = function() {
+    modal.style.display = "block";
+    document.querySelector(".product-image img").src = this.querySelector("img").src;
+    document.getElementById("product-name").innerHTML = this.querySelector(".overlay h1").innerHTML;
+    document.getElementById("product-price").innerHTML = this.querySelector(".overlay p").innerHTML;
+    document.getElementById("product-description").innerHTML = "This hoodie features a graphic print of a colorful nebula, creating an eye-catching design that is sure to turn heads. Made from high-quality materials, this hoodie is soft, warm and durable, making it ideal for everyday wear. The black and white color scheme gives it a modern, minimalist look that will complement any outfit.";
+  }
+}
+/*-------------------------------------------------------------------------------------------------------*/
+// Get the button that opens the modal
+var btn = document.getElementsByClassName("product-link-6");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+for (var i = 0; i < btn.length; i++) {
+  btn[i].onclick = function() {
+    modal.style.display = "block";
+    document.querySelector(".product-image img").src = this.querySelector("img").src;
+    document.getElementById("product-name").innerHTML = this.querySelector(".overlay h1").innerHTML;
+    document.getElementById("product-price").innerHTML = this.querySelector(".overlay p").innerHTML;
+    document.getElementById("product-description").innerHTML = "This hoodie features a graphic print of a countour design with bold colors, creating an eye-catching design that is sure to turn heads. Made from high-quality materials, this hoodie is soft, warm and durable, making it ideal for everyday wear. The black and white color scheme gives it a modern, minimalist look that will complement any outfit.";
+  }
+}
+
+/*----------------------------------------------------------------------------------------------------------------*/
+// Get the button that opens the modal
+var btn = document.getElementsByClassName("product-link-7");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+for (var i = 0; i < btn.length; i++) {
+  btn[i].onclick = function() {
+    modal.style.display = "block";
+    document.querySelector(".product-image img").src = this.querySelector("img").src;
+    document.getElementById("product-name").innerHTML = this.querySelector(".overlay h1").innerHTML;
+    document.getElementById("product-price").innerHTML = this.querySelector(".overlay p").innerHTML;
+    document.getElementById("product-description").innerHTML = "This hoodie features a graphic print of a slash design in black and white, creating an eye-catching design that is sure to turn heads. Made from high-quality materials, this hoodie is soft, warm and durable, making it ideal for everyday wear. The black and white color scheme gives it a modern, minimalist look that will complement any outfit.";
+  }
+}
+
+/*-------------------------------------------------------------------------------------------------------------*/
+// Get the button that opens the modal
+var btn = document.getElementsByClassName("product-link-8");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+for (var i = 0; i < btn.length; i++) {
+  btn[i].onclick = function() {
+    modal.style.display = "block";
+    document.querySelector(".product-image img").src = this.querySelector("img").src;
+    document.getElementById("product-name").innerHTML = this.querySelector(".overlay h1").innerHTML;
+    document.getElementById("product-price").innerHTML = this.querySelector(".overlay p").innerHTML;
+    document.getElementById("product-description").innerHTML = "This hoodie features a graphic print of a large moon with bold colors, creating an eye-catching design that is sure to turn heads. Made from high-quality materials, this hoodie is soft, warm and durable, making it ideal for everyday wear. The black and white color scheme gives it a modern, minimalist look that will complement any outfit.";
+  }
+}
+
+/*---------------------------------------------------------------------------------------------------------*/
+// Get the button that opens the modal
+var btn = document.getElementsByClassName("product-link-9");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+for (var i = 0; i < btn.length; i++) {
+  btn[i].onclick = function() {
+    modal.style.display = "block";
+    document.querySelector(".product-image img").src = this.querySelector("img").src;
+    document.getElementById("product-name").innerHTML = this.querySelector(".overlay h1").innerHTML;
+    document.getElementById("product-price").innerHTML = this.querySelector(".overlay p").innerHTML;
+    document.getElementById("product-description").innerHTML = "This hoodie features a graphic print of a skeleton, creating an eye-catching design that is sure to turn heads. Made from high-quality materials, this hoodie is soft, warm and durable, making it ideal for everyday wear. The black and white color scheme gives it a modern, minimalist look that will complement any outfit.";
+  }
+}
+
+/*--------------------------------------------------------------------------------------------------------*/
+// Get the button that opens the modal
+var btn = document.getElementsByClassName("product-link-10");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+for (var i = 0; i < btn.length; i++) {
+  btn[i].onclick = function() {
+    modal.style.display = "block";
+    document.querySelector(".product-image img").src = this.querySelector("img").src;
+    document.getElementById("product-name").innerHTML = this.querySelector(".overlay h1").innerHTML;
+    document.getElementById("product-price").innerHTML = this.querySelector(".overlay p").innerHTML;
+    document.getElementById("product-description").innerHTML = "This hoodie features a graphic print of splashig colors, creating an eye-catching design that is sure to turn heads. Made from high-quality materials, this hoodie is soft, warm and durable, making it ideal for everyday wear. The black and white color scheme gives it a modern, minimalist look that will complement any outfit.";
+  }
+}
+
+/*-------------------------------------------------------------------------------------------------------------*/
+// Get the button that opens the modal
+var btn = document.getElementsByClassName("product-link-11");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+for (var i = 0; i < btn.length; i++) {
+  btn[i].onclick = function() {
+    modal.style.display = "block";
+    document.querySelector(".product-image img").src = this.querySelector("img").src;
+    document.getElementById("product-name").innerHTML = this.querySelector(".overlay h1").innerHTML;
+    document.getElementById("product-price").innerHTML = this.querySelector(".overlay p").innerHTML;
+    document.getElementById("product-description").innerHTML = "This hoodie features a graphic print of a snowflake design, creating an eye-catching design that is sure to turn heads. Made from high-quality materials, this hoodie is soft, warm and durable, making it ideal for everyday wear. The black and white color scheme gives it a modern, minimalist look that will complement any outfit.";
+  }
+}
+
+/*------------------------------------------------------------------------------------------------------*/
+var btn = document.getElementsByClassName("product-link-12");
+
+		var span = document.getElementsByClassName("close")[0];
+
+		for (var i = 0; i < btn.length; i++) {
+		btn[i].onclick = function() {
+			modal.style.display = "block";
+			document.querySelector(".product-image img").src = this.querySelector("img").src;
+			document.getElementById("product-name").innerHTML = this.querySelector(".overlay h1").innerHTML;
+			document.getElementById("product-price").innerHTML = this.querySelector(".overlay p").innerHTML;
+			document.getElementById("product-description").innerHTML = "This hoodie features a graphic print of an abstract black and blue design, creating an eye-catching design that is sure to turn heads. Made from high-quality materials, this hoodie is soft, warm and durable, making it ideal for everyday wear. The black and white color scheme gives it a modern, minimalist look that will complement any outfit.";
+		}
+		}
+
+		span.onclick = function() {
+		modal.style.display = "none";
+		}
+
+		window.onclick = function(event) {
+		if (event.target == modal) {
+			modal.style.display = "none";
+		}
+		}
